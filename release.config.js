82fd05827b53122e204df3cc02ae3fda0ddb94f2
Stitt <331298@corpaa.aa.com>
2021-08-19 15:30:23 -0500

    fix: dwdwdwdw

--- release.config.js
+++ release.config.js
@@ -8,8 +8,8 @@ module.exports = {
       "@semantic-release/github", 
       {
         assets: [
-          { path: "build.zip", label: "Build"},
-          { path: "coverage.zip", label: "Coverage"}
+          { path: "build.zip"},
+          { path: "coverage.zip"}
         ]
       }
     ] // responsible for creating a github release
