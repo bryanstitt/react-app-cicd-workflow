288945ee3d7d85019c2dabafd1df7788e15f71c4
Stitt <331298@corpaa.aa.com>
2021-08-19 13:27:03 -0500

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -2,8 +2,8 @@ module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/bryanstitt1/react-app-cicd-workflow/",
   plugins: [
-    '@semantic-release/commit-analyzer', // reads commits and decide next version from them
-    '@semantic-release/release-notes-generator', // generate release notes from commit messages
-    '@semantic-release/github' // responsible for creating a github release
-  ]
-}
+    "@semantic-release/commit-analyzer", // reads commits and decide next version from them
+    "@semantic-release/release-notes-generator", // generate release notes from commit messages
+    "@semantic-release/github", // responsible for creating a github release
+  ],
+};
