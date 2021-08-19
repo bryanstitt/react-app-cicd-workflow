812a4b4cf266b41d3ee42570b9e87fec3944d34b
Stitt <331298@corpaa.aa.com>
2021-08-19 15:19:35 -0500

    fix: dwdwdwdw

--- release.config.js
+++ release.config.js
@@ -1,14 +1,31 @@
+// module.exports = {
+//   branches: "master",
+//   repositoryUrl: "https://github.com/bryanstitt1/react-app-cicd-workflow/",
+//   plugins: [
+//     "@semantic-release/commit-analyzer", // reads commits and decide next version from them
+//     "@semantic-release/release-notes-generator", // generate release notes from commit messages
+//     ["@semantic-release/github", {
+//       assets: [
+//         { path: "build.zip", label: "Build"},
+//         { path: "coverage.zip", label: "coverage"}
+//       ]
+//     }], // responsible for creating a github release
+//   ],
+// };
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/bryanstitt1/react-app-cicd-workflow/",
-  plugins: [
-    "@semantic-release/commit-analyzer", // reads commits and decide next version from them
-    "@semantic-release/release-notes-generator", // generate release notes from commit messages
-    ["@semantic-release/github", {
-      assets: [
-        { path: "build.zip", label: "Build"},
-        { path: "coverage.zip", label: "coverage"}
-      ]
-    }], // responsible for creating a github release
-  ],
-};
+  "branches": ["master"],
+  "repositoryUrl": "https://github.com/bryanstitt1/react-app-cicd-workflow/",
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/gitlab",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+}
