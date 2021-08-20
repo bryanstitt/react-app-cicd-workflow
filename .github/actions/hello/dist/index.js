3647c6fbeb0e80268c0a98a97c7f0023b38ca311
Stitt <331298@corpaa.aa.com>
2021-08-20 11:45:01 -0500

    feat: s

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -637,14 +637,24 @@ const core = __webpack_require__(470)
 const github = __webpack_require__(469)
 
 try {
-  throw(new Error("some error message"))
+  // throw(new Error("some error message"))
+
+  core.debug('Debug message')
+  core.warning('Warning message')
+  core.error('Error message')
+
   const name = core.getInput('who-to-greet')
+  core.setSecret(name) // sets varaible to appear in logs as ***
   console.log(`Hello ${name}`);
   
   const time = new Date();
   core.setOutput("time", time.toTimeString())
   
+  core.startGroup('Logging github object') // makes message in log retractable
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup()
+
+  core.exportVariable('HELLO', 'hello') // set env variable
 } catch(error) {
   core.setFailed(error.message);
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,24 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-  throw(new Error("some error message"))
+  // throw(new Error("some error message"))
+
+  core.debug('Debug message')
+  core.warning('Warning message')
+  core.error('Error message')
+
   const name = core.getInput('who-to-greet')
+  core.setSecret(name) // sets varaible to appear in logs as ***
   console.log(`Hello ${name}`);
   
   const time = new Date();
   core.setOutput("time", time.toTimeString())
   
+  core.startGroup('Logging github object') // makes message in log retractable
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup()
+
+  core.exportVariable('HELLO', 'hello') // set env variable
 } catch(error) {
   core.setFailed(error.message);
 }
